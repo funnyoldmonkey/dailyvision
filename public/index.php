1f805ad9731544936e284e2079a02f6f25109b59
Jall Fiel <jall@useamp.com>
2026-04-28 12:33:53 +0800

    Security Lockdown: Restored API key, removed setup scripts, and disabled install routes

--- public/index.php
+++ public/index.php
@@ -13,7 +13,6 @@ $router = new Router();
 
 // Routes
 $router->add('GET', '/', 'HomeController@index');
-$router->add('GET', '/install', 'SetupController@index');
 $router->add('POST', '/api/analyze', 'AiController@analyze');
 $router->add('POST', '/api/visions', 'VisionController@save');
 $router->add('GET', '/v/([a-zA-Z0-9]+)', 'VisionController@view');
